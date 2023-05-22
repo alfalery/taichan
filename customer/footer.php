223b543a1a23aa9199aab1620b614325a6182611
ranggaagm <114371340+ranggaagm@users.noreply.github.com>
2023-05-22 10:38:45 +0700

    Add files via upload

---
+++ customer/footer.php
@@ -0,0 +1,119 @@
+<!-- start: Footer -->
+<div id="footer">
+		
+		<!-- start: Container -->
+		<div class="container">
+			
+			<!-- start: Row -->
+			<div class="row">
+
+				<!-- start: About -->
+				<div class="span3">
+					
+					<h3>Kedai Taichan Ler</h3>
+					<p>
+                    Kedai Taichan Ler merupakan kedai online yang menyediakan aneka makanan lezat dan minuman yang segar dengan metode pembayaran cash on delivery.
+					 </p>
+						
+				</div>
+				<!-- end: About -->
+
+				<!-- start: Photo Stream -->
+				<div class="span3">
+					
+					<h3>Request Menu Baru?</h3>
+                    Jika Anda ingin menyarankan menu baru<br/>
+                    Anda dapat hubungi kami!<br />
+                    Hubungi via whatsapp : <a href="https://api.whatsapp.com/send?phone=6289525634488&text=Hallo%20%20Saya%20Mau%20Request%20Menu%20Baru%20&source=&data=">0895 2563 4488</a><br />
+                    </div>
+				<!-- end: Photo Stream -->
+
+				<div class="span6">
+				
+					<!-- start: Follow Us -->
+					<h3>Follow Us!</h3>
+					<ul class="social-grid">
+						<li>
+							<div class="social-item">				
+								<div class="social-info-wrap">
+									<div class="social-info">
+										<div class="social-info-front social-twitter">
+											<a href="http://twitter.com"></a>
+										</div>
+										<div class="social-info-back social-twitter-hover">
+											<a href="http://twitter.com"></a>
+										</div>	
+									</div>
+								</div>
+							</div>
+						</li>
+						<li>
+							<div class="social-item">				
+								<div class="social-info-wrap">
+									<div class="social-info">
+										<div class="social-info-front social-linkedin">
+											<a href="http://facebook.com"></a>
+										</div>
+										<div class="social-info-back social-linkedin-hover">
+											<a href="http://facebook.com"></a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</li>
+						<li>
+							<div class="social-item">				
+								<div class="social-info-wrap">
+									<div class="social-info">
+										<div class="social-info-front social-dribbble">
+											<a href="http://dribble.com"></a>
+										</div>
+										<div class="social-info-back social-dribble-hover">
+											<a href="http://dribble.com"></a>
+										</div>	
+									</div>
+								</div>
+							</div>
+						</li>
+						<li>
+							<div class="social-item">				
+								<div class="social-info-wrap">
+									<div class="social-info">
+										<div class="social-info-front social-flickr">
+											<a href="http://flickr.com"></a>
+										</div>
+										<div class="social-info-back social-flickr-hover">
+											<a href="http://flickr.com"></a>
+										</div>	
+									</div>
+								</div>
+							</div>
+						</li>
+					</ul>
+				
+				</div>
+				
+			</div>
+			<!-- end: Row -->	
+			
+		</div>
+		<!-- end: Container  -->
+
+	</div>
+	<!-- end: Footer -->
+
+	<!-- start: Copyright -->
+	<div id="copyright">
+	
+		<!-- start: Container -->
+		<div class="container">
+		
+			<p>
+			</a>
+			</p>
+	
+		</div>
+		<!-- end: Container  -->
+		
+	</div>	
+	<!-- end: Copyright -->
